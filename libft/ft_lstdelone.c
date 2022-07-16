ba34005a266a093adc53f8415ec0a013c6084037
YOOHOO0 <yoohoo_030@naver.com>
2022-07-17 00:43:29 +0900

    .

--- libft/ft_lstdelone.c
+++ libft/ft_lstdelone.c
@@ -14,10 +14,6 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	t_list	*temp;
-
-	temp = lst;
-	lst = lst->next;
-	del(temp->content);
-	free(temp);
+	del(lst->content);
+	free(lst);
 }
