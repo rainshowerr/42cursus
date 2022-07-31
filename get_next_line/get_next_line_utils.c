06c82dd5b6a5651c75866cd9a662b7c348f56125
YOOHOO0 <yoohoo030@naver.com>
2022-07-31 17:05:45 +0900

    .

---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seoshin <seoshin@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/22 18:36:58 by seoshin           #+#    #+#             */
+/*   Updated: 2022/07/30 22:01:17 by seoshin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char	*src)
+{
+	size_t	k;
+
+	if (src == 0)
+		return (0);
+	k = 0;
+	while (src[k] != '\0')
+		k++;
+	return (k);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	if (s1 == 0)
+		return (ft_strdup(s2));
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	str = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (!str)
+		return (0);
+	i = 0;
+	while (i < s1_len)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (j < s2_len)
+		str[i++] = s2[j++];
+	str[i] = '\0';
+	free(s1);
+	return (str);
+}
+
+char	*ft_strdup(const char *src)
+{
+	int		i;
+	char	*str;
+
+	str = (char *)malloc((ft_strlen(src) + 1) * sizeof(char));
+	if (!str)
+		return (0);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		str[i] = src[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int		idx;
+	char	cc;
+	int		i;
+
+	if (!s)
+		return (0);
+	cc = (char)c;
+	idx = 0;
+	i = (int)ft_strlen(s);
+	while (idx <= i)
+	{
+		if (s[idx] == cc)
+			return ((char *)&s[idx]);
+		idx++;
+	}
+	return (0);
+}
